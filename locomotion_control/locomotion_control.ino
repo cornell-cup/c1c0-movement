6f9ad65bce49bea13d1ce136a6f6c079f9c2ec29
Tiffany Guo <tiffanyguo@dhcp-vl2042-12171.redrover.cornell.edu>
2021-09-14 21:14:30 -0400

    motor moving

--- locomotion_control/locomotion_control.ino
+++ locomotion_control/locomotion_control.ino
@@ -7,12 +7,20 @@
   
  */
 
-
-
 void setup() {
-  
+  pinMode(3, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+  analogWrite(3, 25); //253 max
+  digitalWrite(8, HIGH);
+  int speed = analogRead(A0); //rpm scaled by 15
+  int avspeed = analogRead(A1);
+  Serial.println(speed);
+  Serial.println(avspeed);
+  Serial.println("");
 }
