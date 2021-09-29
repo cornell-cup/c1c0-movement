ecfe67d943cbba800a069a0e37c2ad54d20886d2
kyleinfantino <kyleinfantino@gmail.com>
2021-09-28 20:17:54 -0400

    Adding pid library

--- locomotion_control/locomotion_control.ino
+++ locomotion_control/locomotion_control.ino
@@ -1,13 +1,12 @@
 /*
-  - Read x and y values from Jetson
+  - Read x and y values from Jetson (0.0-1.0,0.0-1.0)
   - Calculate desired angle/motor speeds
   - Move motors
   - Read encoder values
   - Change speeds based on feedback
   
  */
-
-#define cw(x) ((x) <= 0 ? 0 : 1)
+ //#include <PID_v1.h>
 
 int pwmPinR = 11;
 int cwPinR = 12;
@@ -17,7 +16,7 @@ int pwmPinL = 3;
 int cwPinL = 7;
 int ccwPinL = 8;
 int rpmPinL = A1;
-int maxrpm = 253; //placeholder
+int maxpwm = 253;
  
 
 void setup() {
@@ -34,40 +33,34 @@ void setup() {
 }
 
 void loop() {
-  //receive direction input
+  //receive direction input of form (float,float). Ex: (1.0,1.0) means forward full speed
   String incomingStr = "(1.0, 1.0)";
   
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingStr = Serial.read();
   }
-    // Serial.print("I received: ");
-    // Serial.println(incomingStr);
-  //(a,a)
   
   float left = incomingStr.substring(1, incomingStr.indexOf(',')).toFloat();
   float right = incomingStr.substring(incomingStr.indexOf(',')+1, incomingStr.length()-1).toFloat();
+  bool cwR = (right > 0);
+  bool cwL = (left < 0);
   
-  Serial.println(left);
-  Serial.println(right);
-  Serial.println("");
+//  Serial.println(left);
+//  Serial.println(right);
+//  Serial.println("");
   
-  int leftrpm = left*maxrpm;
-  int rightrpm = right*maxrpm;
-  analogWrite(pwmPinL,leftrpm); //253 max
-  analogWrite(pwmPinR,rightrpm); //253 max
-  digitalWrite(cwPinR, HIGH); //Direction --forward
-  digitalWrite(cwPinL, HIGH); //Direction
-  digitalWrite(ccwPinR, LOW); //Direction
-  digitalWrite(ccwPinL, LOW); //Direction
+  int leftpwm = left*maxpwm;
+  int rightpwm = right*maxpwm;
+  analogWrite(pwmPinL,leftpwm); //253 max
+  analogWrite(pwmPinR,rightpwm); //253 max
+  digitalWrite(cwPinR, cwR); //Direction --forward
+  digitalWrite(cwPinL, cwL); //Direction
+  digitalWrite(ccwPinR, ~cwR); //Direction
+  digitalWrite(ccwPinL, ~cwL); //Direction
   
 
   //Control
-//  int speed1 = analogRead(rpmPin1);
-//  int rpm1 = anlgToRpm(speed1);
-//  int speed2 = analogRead(rpmPin2);
-//  int rpm2 = anlgToRpm(speed2);
-  // rpmleft, rpmright, encleft, encright
   // ratio L/R
 //   enc1 = [10, 65000, 10] //need wrap around case
 //   enc2 = [10, 21. 32]
@@ -92,20 +85,3 @@ int anlgToRpm(int anlg){ //2v = 0rpm, 4v = 8000rpm, 0v = -8000
   anlg = anlg-512; //[-512, 512]
   return anlg*15.625; //[-8000, 8000]
 }
-
-//void parseData() {
-//
-//    // split the data into its parts
-//    
-//  char * strtokIndx; // this is used by strtok() as an index
-//  
-//  strtokIndx = strtok(receivedChars,",");      // get the first part - the string
-//  strcpy(message1, strtokIndx); // copy it to messageFromPC
-//  
-//  strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-//  integerFromPC = atoi(strtokIndx);     // convert this part to an integer
-//  
-//  strtokIndx = strtok(NULL, ","); 
-//  floatFromPC = atof(strtokIndx);     // convert this part to a float
-//
-//}
