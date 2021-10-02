f67ce06275c9ebfde2b5c9c4fb3b1a9d79c1d4b2
Cornell Cup Robotics <bas335@cornell.edu>
2021-10-02 15:01:30 -0400

    jetson encoded message to Arduino

--- c1c0-movement/Locomotion/c1c0_liam_jonas_protocol_test2.py
+++ c1c0-movement/Locomotion/c1c0_liam_jonas_protocol_test2.py
@@ -5,11 +5,17 @@ ser = serial.Serial(
 	port = '/dev/ttyTHS1',
 	baudrate = 9600,
 )
-type1 = "stuf"
-data = [1,2,3]
+type1 = "ON"
+data = "ON"
 while(1):
 	for i in range(6):
-		print(data)
-		msg = r2p.encode(bytes(type1, 'utf-8'),bytearray(data))
+		
+		print("ON")
+		msg = r2p.encode(b"ON",b"\x00a\x00a")
 		ser.write(msg)
-		time.sleep(1)
+		time.sleep(3)
+		print("OFF")
+		msg = r2p.encode(b"OFF",b"\x00a\x00a")
+		ser.write(msg)
+		time.sleep(3)
+		
