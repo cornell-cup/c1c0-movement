6e604f80fd82cf0770ee4f5c22eac37bbe9f5b86
LK1007 <kainliam0@gmail.com>
2021-10-02 12:23:08 -0400

    Changed name of test folder

---
+++ c1c0-movement/Locomotion/c1c0_liam_jonas_protocol_test/c1c0_liam_jonas_protocol_test.ino
@@ -0,0 +1,20 @@
+#include "Protocol.h"
+uint8_t fsm_buffer[256];
+r2pf_t fsm;
+void setup() {
+  fsm = r2pf_init(fsm_buffer, 256);
+  Serial1.begin(9600);
+  pinMode(13,OUTPUT);
+}
+char s = 'a';
+void loop() {
+
+    if(Serial1.available() > 0)
+    {
+      uint8_t b = Serial.read();
+      r2pf_read(&fsm, b);
+    }
+    if (fsm.done)
+    digitalWrite(13,HIGH);
+
+}
