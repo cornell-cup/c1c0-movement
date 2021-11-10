32dd9ff2c207a8285d1d62c89cd7d060c2c85805
ki5953 <kyleinfantino@gmail.com>
2021-11-09 22:10:47 -0500

    fixed decode test

--- c1c0-movement/Locomotion/Modified Bus Protocol/modified_protocol_test_jetson_sending.py
+++ c1c0-movement/Locomotion/Modified Bus Protocol/modified_protocol_test_jetson_sending.py
@@ -7,13 +7,35 @@ ser = serial.Serial(
     port = '/dev/ttyTHS1',
     baudrate = 9600,
 )
-address = 4;
-type1 = b"ON"
-data = b"\x0a\x0a"
+address = 4
+address2 = 8
+data = b"\x09\x09"
+data2 = b"\x0a\x0a"
+data3 = b"\x0b\x0b" 
+data4 = b"\x0f\x0f" 
 while(1):    
-    print("ON")
-    msg = r2p.encode(b"ON",address.to_bytes(1,'big'),data)
+    print("controller1")
+    msg = r2p.encode(b"Lite",address.to_bytes(1,'big'),data)
     print(msg)
     ser.write(msg)
-    time.sleep(3)
-        
+    time.sleep(2)
+    
+    print("controller2")
+    msg = r2p.encode(b"Dark",address2.to_bytes(1,'big'),data2)
+    print(msg)
+    ser.write(msg)
+    time.sleep(2)
+
+    print("controller1")
+    msg = r2p.encode(b"Lite",address.to_bytes(1,'big'),data3)
+    print(msg)
+    ser.write(msg)
+    time.sleep(2)
+
+    print("controller2")
+    msg = r2p.encode(b"Dark",address2.to_bytes(1,'big'),data4)
+    print(msg)
+    ser.write(msg)
+    time.sleep(2)
+
+
