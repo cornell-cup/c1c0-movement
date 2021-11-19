8bc1b7b9b1b609d2ff1cc3284642f1558639ce41
ki5953 <kyleinfantino@gmail.com>
2021-11-18 19:37:10 -0500

    finished Jetson sending test

--- c1c0-movement/Locomotion/Modified Bus Protocol/modified_protocol_test_jetson_sending.py
+++ c1c0-movement/Locomotion/Modified Bus Protocol/modified_protocol_test_jetson_sending.py
@@ -1,4 +1,4 @@
-
+import random
 import serial
 import time 
 #import R2Protocol2 as r2p
@@ -13,29 +13,22 @@ data = b"\x09\x09"
 data2 = b"\x0a\x0a"
 data3 = b"\x0b\x0b" 
 data4 = b"\x0f\x0f" 
-while(1):    
-    print("controller1")
-    msg = r2p.encode(b"Lite",address.to_bytes(1,'big'),data)
-    print(msg)
-    ser.write(msg)
-    time.sleep(2)
-    
-    print("controller2")
-    msg = r2p.encode(b"Dark",address2.to_bytes(1,'big'),data2)
-    print(msg)
-    ser.write(msg)
-    time.sleep(2)
-
-    print("controller1")
-    msg = r2p.encode(b"Lite",address.to_bytes(1,'big'),data3)
-    print(msg)
-    ser.write(msg)
-    time.sleep(2)
-
-    print("controller2")
-    msg = r2p.encode(b"Dark",address2.to_bytes(1,'big'),data4)
-    print(msg)
-    ser.write(msg)
-    time.sleep(2)
+while(1):
+    for i in range(0,3):
+        print("controller1")
+        msg = r2p.encode(b"Lite",(2+i).to_bytes(1,'big'),data3)
+        msg2 = r2p.encode(b"Lite",(2-i).to_bytes(1,'big'),data3)
+        print(msg)
+        ser.write(msg)
+        ser.write(msg2)
+        time.sleep(.2)
+    for i in range(0,3):
+        print("controller1")
+        msg = r2p.encode(b"Lite", (2+i).to_bytes(1,'big'),data)
+        msg2 = r2p.encode(b"Lite", (2-i).to_bytes(1,'big'),data)
+        print(msg)
+        ser.write(msg)
+        ser.write(msg2)
+        time.sleep(.2)
 
 
