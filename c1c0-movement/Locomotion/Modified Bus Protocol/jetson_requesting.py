7d9c052c6dff8bdd05e09dd22f328dae6507a1cb
ki5953 <kyleinfantino@gmail.com>
2021-11-20 15:52:48 -0500

    better request test jetson

--- c1c0-movement/Locomotion/Modified Bus Protocol/jetson_requesting.py
+++ c1c0-movement/Locomotion/Modified Bus Protocol/jetson_requesting.py
@@ -3,6 +3,8 @@ import random
 import serial
 import time 
 import modified_protocol2 as r2p
+import select
+import sys
 ser = serial.Serial(
         port = '/dev/ttyTHS1',
         baudrate = 9600,
@@ -10,12 +12,25 @@ ser = serial.Serial(
 address = 4
 address2 = 8
 data = b"\x09\x09"
+i = 0
 while(1):
-		req = input();
-		if(int(req) == 1):
-				print("sending ...")
-				msg = r2p.encode(b"rqst",(4).to_bytes(1,'big'),data)
-				ser.write(msg)
-		x = ser.read_until(expected = b'\xd2\xe2\xf2')
+	print("Start of cycle")
+	print(i)
+	msg = r2p.encode(b"rqst",(4).to_bytes(1,'big'),data)
+	ser.write(msg)
+	time.sleep(.5)
+	if(i % 5 == 0):
+		print("sending ...")
+	while ser.in_waiting:
+		print("Reading Message")
+		x = ser.read(20)
+		##x = ser.read_until(expected = b'\xd2\xe2\xf2' )
 		s = r2p.decode(x)
- 
+		print("Message received: ", s)
+	i = i + 1
+	print()
+	time.sleep(.5)
+	
+	
+
+
--- c1c0-movement/Locomotion/Modified Bus Protocol/modified_protocol_test_jetson_sending.py
+++ c1c0-movement/Locomotion/Modified Bus Protocol/modified_protocol_test_jetson_sending.py
@@ -14,21 +14,21 @@ data2 = b"\x0a\x0a"
 data3 = b"\x0b\x0b" 
 data4 = b"\x0f\x0f" 
 while(1):
-    for i in range(0,3):
+    for i in range(0,5):
         print("controller1")
-        msg = r2p.encode(b"Lite",(2+i).to_bytes(1,'big'),data3)
-        msg2 = r2p.encode(b"Lite",(2-i).to_bytes(1,'big'),data3)
+        msg = r2p.encode(b"Lite",(i).to_bytes(1,'big'),data3)
+        #msg2 = r2p.encode(b"Lite",(i).to_bytes(1,'big'),data3)
         print(msg)
         ser.write(msg)
-        ser.write(msg2)
+        #ser.write(msg2)
         time.sleep(.2)
-    for i in range(0,3):
+    for i in range(0,5):
         print("controller1")
-        msg = r2p.encode(b"Lite", (2+i).to_bytes(1,'big'),data)
-        msg2 = r2p.encode(b"Lite", (2-i).to_bytes(1,'big'),data)
+        msg = r2p.encode(b"Lite", (i).to_bytes(1,'big'),data)
+        #msg2 = r2p.encode(b"Lite", (i).to_bytes(1,'big'),data)
         print(msg)
         ser.write(msg)
-        ser.write(msg2)
+        #ser.write(msg2)
         time.sleep(.2)
 
 
