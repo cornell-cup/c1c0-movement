a05b5c7de931d1e5d1cfa3616179e6c83a3a00f7
CornellCup <jwt95@cornell.edu>
2021-11-20 15:46:49 -0500

    Arduino to Jetson R2 Interface Updates (work in progres)

--- Arduino_to_JetsonR2.py
+++ Arduino_to_JetsonR2.py
@@ -8,9 +8,10 @@ Arduino to Jetson Communication with R2 Protocol
 
 sys.path.insert(0, './c1c0-movement/Locomotion/')
 import R2Protocol2 as r2p
+file1 = open("file1.txt", "w")
 
 ser = None
-
+ 
 def init_serial(port,baud):
 	'''
 	Initializes the serial port, usually set baud to 9600
@@ -29,14 +30,19 @@ def close_serial():
 if __name__ =='__main__':
 	init_serial('/dev/ttyTHS1', 9600)
 	
+	#how can I get this to print the hex value and not the ASCII
 	try:
             while True:
                 start = time.time()
                 ser_msg = ser.read(22) # length of message plus 16
                 for i in range(0, len(ser_msg), 32):
                     r2p.decode(ser_msg[i:i+32])
+                    #print(ser_msg[i:i+32])
                     print(ser_msg[i:i+32])
-				
+                    file1.write(str(ser_msg[i:i+32]))
+			
+               
+	
 	except KeyboardInterrupt:
             ser.close()
 			
