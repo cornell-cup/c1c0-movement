3a63821c792d2ecdc965dd2027f1d7dae6081c65
ki5953 <kyleinfantino@gmail.com>
2021-11-30 21:26:29 -0500

    added request system jetson working

--- c1c0-movement/Locomotion/Modified Bus Protocol/jetson_requesting.py
+++ c1c0-movement/Locomotion/Modified Bus Protocol/jetson_requesting.py
@@ -5,31 +5,47 @@ import time
 import modified_protocol2 as r2p
 import select
 import sys
+import threading
+import Jetson.GPIO as GPIO
 ser = serial.Serial(
         port = '/dev/ttyTHS1',
         baudrate = 9600,
 )
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
 address = 4
 address2 = 8
 data = b"\x09\x09"
+data2 = b"\x08\x08"
 i = 0
-while(1):
-	print("Start of cycle")
-	print(i)
-	msg = r2p.encode(b"rqst",(4).to_bytes(1,'big'),data)
-	ser.write(msg)
-	time.sleep(.5)
-	if(i % 5 == 0):
-		print("sending ...")
-	while ser.in_waiting:
-		print("Reading Message")
-		x = ser.read(20)
-		##x = ser.read_until(expected = b'\xd2\xe2\xf2' )
+
+def send():
+	while(1):
+		msg = r2p.encode(b"rqst",(4).to_bytes(1,'big'),data)
+		ser.write(msg)
+		print("sending " + str(msg))
+		time.sleep(1)
+		msg = r2p.encode(b"rqst",(4).to_bytes(1,'big'),data2)
+		ser.write(msg)
+		time.sleep(1)
+		
+def receive():
+	while(1):
+		x = ser.read_until(expected = b'\xd2\xe2\xf2' )
 		s = r2p.decode(x)
-		print("Message received: ", s)
-	i = i + 1
-	print()
-	time.sleep(.5)
+		print(str(s[0]))
+		time.sleep(1)
+		if(s[3] == 4):
+			GPIO.output(7,GPIO.HIGH)
+			time.sleep(.5)
+			GPIO.output(7,GPIO.LOW)
+
+print("Start of cycle")
+x =threading.Thread(target=send)
+y =threading.Thread(target=receive)
+x.start()
+y.start()
+
 	
 	
 
