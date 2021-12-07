418a0249cf963b97c55f44aded0386af7d8cf8ca
ki5953 <kyleinfantino@gmail.com>
2021-12-07 18:43:15 -0500

    almost finished requesting system

--- c1c0-movement/Locomotion/Modified Bus Protocol/modified_protocol_test_jetson_sending.py
+++ c1c0-movement/Locomotion/Modified Bus Protocol/modified_protocol_test_jetson_sending.py
@@ -14,7 +14,7 @@ data2 = b"\x0a\x0a"
 data3 = b"\x0b\x0b" 
 data4 = b"\x0f\x0f" 
 while(1):
-    for i in range(0,5):
+    for i in range(0,6):
         print("controller1")
         msg = r2p.encode(b"Lite",(i).to_bytes(1,'big'),data3)
         #msg2 = r2p.encode(b"Lite",(i).to_bytes(1,'big'),data3)
@@ -22,7 +22,7 @@ while(1):
         ser.write(msg)
         #ser.write(msg2)
         time.sleep(.2)
-    for i in range(0,5):
+    for i in range(0,6):
         print("controller1")
         msg = r2p.encode(b"Lite", (i).to_bytes(1,'big'),data)
         #msg2 = r2p.encode(b"Lite", (i).to_bytes(1,'big'),data)
