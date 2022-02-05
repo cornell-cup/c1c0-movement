b9e8669ef195d2a0f5833f6bed45e12bc00c6112
Liam Kain <kainliam0@gmail.com>
2022-02-05 15:09:03 -0500

    Made bus protocol work correctly

--- c1c0-movement/Locomotion/Modified Bus Protocol/modified_protocol_test_arduino_decode_receiving/modified_protocol_test_arduino_decode_receiving.ino
+++ c1c0-movement/Locomotion/Modified Bus Protocol/modified_protocol_test_arduino_decode_receiving/modified_protocol_test_arduino_decode_receiving.ino
@@ -3,7 +3,7 @@
 
 const uint8_t dataLength = 2; // since data_len may be changed by decode, this ensures all assumed data lengths are specified manually
 uint16_t checksum; //integer for checksum to be inserted into
-uint8_t address = 0; // ID address for this microcontroller. If the message does not contain this address of 4, the message will not be processed
+uint8_t address = 3; // ID address for this microcontroller. If the message does not contain this address of 4, the message will not be processed
 uint8_t recv_buffer[R2P_HEADER_SIZE + dataLength]; // this is the receiving buffer which the data will be put into, the data is 2 bytes long, so the buffer is 2 + the header size
 uint32_t buffer_len = R2P_HEADER_SIZE + dataLength; 
 char type[5]; //character array which the type literal will be inserted into
@@ -12,7 +12,7 @@ uint32_t data_len; // integer for length of data to be inserted into
 uint8_t datalast;
 int i = 0;
 void setup() {
-
+  Serial.begin(9600);
   Serial1.begin(9600);
   pinMode(13,OUTPUT);
 
@@ -38,6 +38,13 @@ void printmsg(){
 
   
 }
+void printBuff(uint8_t buf[], int len){
+  for(int i = 0; i < len; i++){
+    Serial.print(buf[i],HEX);    
+  }
+
+  Serial.println();
+}
 void loop() {
     if(Serial1.available() > 0) //checks if there is data in the serial buffer to be read
     {
@@ -45,15 +52,17 @@ void loop() {
       datalast = data[0];
       
       Serial1.readBytes(recv_buffer,R2P_HEADER_SIZE + dataLength); // reads the buffer data storing a buffer_len length of data in in recv_buffer
+      printBuff(recv_buffer,R2P_HEADER_SIZE + dataLength);
       r2p_decode(recv_buffer,address,buffer_len,&checksum,type,data, &data_len, &t); // decoding received data
       //r2p_decode(recv_buffer,buffer_len,&checksum,type,data, &data_len);
+      printmsg();
       if(datalast != data[0]){
          if(data[0] == 11)
            digitalWrite(13,HIGH);
          else
            digitalWrite(13,LOW);
       }
-      
+
       }
 
    }
