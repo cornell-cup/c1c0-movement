4b4156ce53fa5b14ce189ccc825df9fd5e094d03
ki5953 <kyleinfantino@gmail.com>
2022-02-10 18:39:49 -0500

    More organization

---
+++ c1c0-movement/Locomotion/Modified Bus Protocol/Arduino/arduino_decode_receiving/modified_protocol_test_arduino_decode_receiving.ino
@@ -0,0 +1,68 @@
+#include "C:\Users\Liam Kain\Documents\ProjectTeam\c1c0-movement\c1c0-movement\Locomotion\Modified Bus Protocol\modified_protocol.h"
+//#include "C:\Users\Liam Kain\Documents\ProjectTeam\c1c0-movement\c1c0-movement\Locomotion\R2Protocol.h";
+
+const uint8_t dataLength = 2; // since data_len may be changed by decode, this ensures all assumed data lengths are specified manually
+uint16_t checksum; //integer for checksum to be inserted into
+uint8_t address = 3; // ID address for this microcontroller. If the message does not contain this address of 4, the message will not be processed
+uint8_t recv_buffer[R2P_HEADER_SIZE + dataLength]; // this is the receiving buffer which the data will be put into, the data is 2 bytes long, so the buffer is 2 + the header size
+uint32_t buffer_len = R2P_HEADER_SIZE + dataLength; 
+char type[5]; //character array which the type literal will be inserted into
+uint8_t data[2]; //the array which data will be inserted into
+uint32_t data_len; // integer for length of data to be inserted into
+uint8_t datalast;
+int i = 0;
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  pinMode(13,OUTPUT);
+
+  while (Serial1.available() > 0){
+    Serial1.read();
+  }
+  delay(100);
+}
+uint16_t t;
+void printmsg(){
+  Serial.print("Address: ") ;
+  Serial.println(address);
+  Serial.print("Type : ");
+  Serial.println(type);
+  Serial.print("Length: ") ;
+  Serial.println(data_len);
+  Serial.println("Data : ");
+   for(int i=0; i<data_len; i++){
+     Serial.print(data[i]);
+     Serial.print("  ");
+     }
+   Serial.println(" ");
+
+  
+}
+void printBuff(uint8_t buf[], int len){
+  for(int i = 0; i < len; i++){
+    Serial.print(buf[i],HEX);    
+  }
+
+  Serial.println();
+}
+void loop() {
+    if(Serial1.available() > 0) //checks if there is data in the serial buffer to be read
+    {
+      
+      datalast = data[0];
+      
+      Serial1.readBytes(recv_buffer,R2P_HEADER_SIZE + dataLength); // reads the buffer data storing a buffer_len length of data in in recv_buffer
+      printBuff(recv_buffer,R2P_HEADER_SIZE + dataLength);
+      r2p_decode(recv_buffer,address,buffer_len,&checksum,type,data, &data_len, &t); // decoding received data
+      //r2p_decode(recv_buffer,buffer_len,&checksum,type,data, &data_len);
+      printmsg();
+      if(datalast != data[0]){
+         if(data[0] == 11)
+           digitalWrite(13,HIGH);
+         else
+           digitalWrite(13,LOW);
+      }
+
+      }
+
+   }
---
+++ c1c0-movement/Locomotion/Modified Bus Protocol/Arduino/arduino_sending/arduino_sending.ino
@@ -0,0 +1,35 @@
+#include "C:\Users\Liam Kain\Documents\ProjectTeam\c1c0-movement\c1c0-movement\Locomotion\Modified Bus Protocol\modified_protocol.h"
+int ID = 9;
+uint8_t send_buffer[256];
+void send(char type[5], uint8_t address, const uint8_t* data, uint32_t data_len) {
+  Serial.println(int(address));
+  uint32_t written = r2p_encode(type, address, data, data_len, send_buffer, 256);
+  printBuff(send_buffer,written);
+  Serial1.write(send_buffer, written);
+}
+void printBuff(uint8_t buf[], int len){
+  for(int i = 0; i < len; i++){
+    Serial.print(buf[i],HEX);    
+  }
+
+  Serial.println();
+}
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  pinMode(13,OUTPUT);
+}
+uint8_t data[] = {0x00d,0x00b,0x00c};
+uint8_t data2[] = {0x00c,0x00b,0x00d};
+char s = 'a';
+void loop() {
+    //sending byte array "STUFF" to address 8  
+   send("ON", ID, data, 8);
+   delay(2000);
+   //sending byte array "STUFF" to address 10
+   send("ON", ID, data2, 8);
+   delay(2000);
+   
+}
+
+  // put your main code here, to run repeatedly:
