442f7ac7e654c146006f2c5128f0c5ea3fa571ed
Liam Kain <kainliam0@gmail.com>
2022-02-10 20:00:46 -0500

    Arduino unit tests work

--- c1c0-movement/Locomotion/Modified Bus Protocol/Arduino/arduino_unit_test/arduino_unit_test.ino
+++ c1c0-movement/Locomotion/Modified Bus Protocol/Arduino/arduino_unit_test/arduino_unit_test.ino
@@ -5,9 +5,9 @@
 Dark: address = 8
 Lite: address = 4
 */
-const uint8_t dataLength = 2; // since data_len may be changed by decode, this ensures all assumed data lengths are specified manually
+const uint8_t dataLength = 200; // since data_len may be changed by decode, this ensures all assumed data lengths are specified manually
 uint16_t checksum; //integer for checksum to be inserted into
-uint8_t address = 4; // ID address for this microcontroller. If the message does not contain this address of 4, the message will not be processed
+uint8_t address = 3; // ID address for this microcontroller. If the message does not contain this address of 4, the message will not be processed
 uint8_t recv_buffer[R2P_HEADER_SIZE + dataLength]; // this is the receiving buffer which the data will be put into, the data is 2 bytes long, so the buffer is 2 + the header size
 uint32_t buffer_len = R2P_HEADER_SIZE + dataLength; 
 char type[5]; //character array which the type literal will be inserted into
@@ -16,15 +16,16 @@ uint32_t data_len; // integer for length of data to be inserted into
 uint8_t datalast;
 uint8_t send_buffer[256];
 void send(char type[5], uint8_t address, const uint8_t* data, uint32_t data_len) {
-  Serial.println("sending stuff");
+  //Serial.println("sending stuff");
   uint32_t written = r2p_encode(type, address, data, data_len, send_buffer, 256);
   Serial1.write(send_buffer, written);
 }
 void setup() {
   Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial1.begin(115200);
   pinMode(13,OUTPUT);
 }
+uint16_t t;
 char s = 'a';
 void printmsg(){
   Serial.print("Address: ") ;
@@ -41,24 +42,22 @@ void printmsg(){
   Serial.println(" ");
   
 }
-uint8_t senddata[] = {0x00d,0x00b,0x00c};
+uint8_t senddata[] = {0x08, 0x08, 0x08};
 uint8_t data2[] = {0x00c,0x00b,0x00d};
 void loop() {
     if(Serial1.available() > 0) //checks if there is data in the serial buffer to be read
     {
       Serial1.readBytes(recv_buffer,R2P_HEADER_SIZE + dataLength); // reads the buffer data storing a buffer_len length of data in in recv_buffer
-      r2p_decode(recv_buffer,address,buffer_len,&checksum,type,data, &data_len, &isRequest ); // decoding received data
+      r2p_decode(recv_buffer,address,buffer_len,&checksum,type,data, &data_len, &t); // decoding received data
       }
-    printmsg()
+    //printmsg();
       if(strncmp(type,"rqst",4)==0){
         if(data[0] == 8)
-          send("OFF", address, senddata, 8);
+          send("OFF", address, senddata, 3);
         else if(data[0] == 9)
-          send("ON", address, senddata, 8);
+          send("ON", address, senddata, 3);
         memcpy(type,"llll", 4);
       }
       
-
-      delay(100);
       
    }
