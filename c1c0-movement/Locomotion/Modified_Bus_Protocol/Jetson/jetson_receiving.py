0b6c71873d40ab8491d09fdd4fe423bcbd397b5c
ki5953 <kyleinfantino@gmail.com>
2022-03-05 17:39:39 -0500

    added dynamic arrays jetson

--- c1c0-movement/Locomotion/Modified_Bus_Protocol/Jetson/jetson_receiving.py
+++ c1c0-movement/Locomotion/Modified_Bus_Protocol/Jetson/jetson_receiving.py
@@ -4,13 +4,11 @@ import time
 import modified_protocol2 as r2p
 ser = serial.Serial(
 	port = '/dev/ttyTHS1', ##setting up jetson serial port at 9600 baud rate
-	baudrate = 9600,
+	baudrate = 38400,
 )
 ID = 4
 while(1): ## loop takes in data sent from jetson. In the decode method, the message is parsed if the ID variable matches the address of the sent message
 	x = ser.read_until(expected = b'\xd2\xe2\xf2')
-	(typ,msg,status,address) = r2p.decode(x)
-	typ = typ.decode()
-	s = typ,msg,status,address
+	s = r2p.decode(x)
 	print(s)
 
