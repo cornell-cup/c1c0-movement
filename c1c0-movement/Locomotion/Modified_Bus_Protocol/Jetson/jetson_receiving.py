19840c8c461c975b53f57dc9d62763f51a60a6d3
ki5953 <kyleinfantino@gmail.com>
2022-03-17 19:58:25 -0400

    More modbus programs

--- c1c0-movement/Locomotion/Modified_Bus_Protocol/Jetson/jetson_receiving.py
+++ c1c0-movement/Locomotion/Modified_Bus_Protocol/Jetson/jetson_receiving.py
@@ -4,12 +4,14 @@ import time
 import modified_protocol2 as r2p
 ser = serial.Serial(
 	port = '/dev/ttyTHS1', ##setting up jetson serial port at 9600 baud rate
-	baudrate = 9600,
+	baudrate = 115200,
 )
 ID = 4
+ser.reset_input_buffer()
 while(1): ## loop takes in data sent from jetson. In the decode method, the message is parsed if the ID variable matches the address of the sent message
-	x = ser.read_until(expected = b'\xd2\xe2\xf2')
-    #print(x)
-	s = r2p.decode(x)
-	print(s)
+    ##x = ser.read(13)
+    x = ser.read_until(expected = b'\xd2\xe2\xf2')
+    print('undecoded:',x)
+    s = r2p.decode(x)
+    print('decoded:',s)
 
