d097f37cded52e63b1df9b9b05124771d8c801d0
Liam Kain <kainliam0@gmail.com>
2022-04-26 19:53:01 -0400

    Made mega work with bus

--- c1c0-movement/Locomotion/Modified_Bus_Protocol/Jetson/jetson_receiving.py
+++ c1c0-movement/Locomotion/Modified_Bus_Protocol/Jetson/jetson_receiving.py
@@ -9,10 +9,10 @@ ser = serial.Serial(
 ID = 4
 ser.reset_input_buffer()
 while(1): ## loop takes in data sent from jetson. In the decode method, the message is parsed if the ID variable matches the address of the sent message
-    #x = ser.read_until(expected = b'\xd2\xe2\xf2')
+    x = ser.read_until(expected = b'\xd2\xe2\xf2')
 
     #x = x[-20:]
-    x = ser.read(20)
+    #x = ser.read(20)
     print('undecoded:',x)
     s = r2p.decode(x)
     print('decoded:',s)
